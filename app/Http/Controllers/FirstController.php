72b0dc46f36570bf93b0c7ad3e9a8c9d9c3d9fb1
unknown <mandoooman@gmail.com>
2020-06-13 22:29:16 +0200

    first install laravel

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    //
+    public public function FunctionName(Type $var = null)
+    {
+        
+    }
+}
